50b3b11f1566710df5bdb1c8979ddf1cd8659edd
jphao <1158608869seu@gmail.com>
2021-11-15 00:17:37 +0800

    init project

---
+++ src/test/java/com/olivine/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivine.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
