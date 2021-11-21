1ecf05a8fc5c72d7a6980e5146c22f40af0a248e
jphao <1158608869seu@gmail.com>
2021-11-21 16:55:04 +0800

    Mybatis application class

---
+++ spring-boot-mybatis/src/test/java/com/olivine/mybatis/SpringBootMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivine.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
