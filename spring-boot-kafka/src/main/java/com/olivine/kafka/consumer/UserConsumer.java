c0711306bb90b6e97fca5ed881a784042e197384
jphao <1158608869seu@gmail.com>
2021-11-23 20:01:28 +0800

    spring-boot-kafka  消费者指定分区消费

--- spring-boot-kafka/src/main/java/com/olivine/kafka/consumer/UserConsumer.java
+++ spring-boot-kafka/src/main/java/com/olivine/kafka/consumer/UserConsumer.java
@@ -3,6 +3,8 @@ package com.olivine.kafka.consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +21,18 @@ public class UserConsumer {
     public void receive(ConsumerRecord<String, String> record, Acknowledgment ack){
 
         log.info("record:{}", record);
+        // 手动提交
         ack.acknowledge();
     }
 
+
+    // 指定分区消费
+    @KafkaListener(groupId = "user2", topicPartitions = {@TopicPartition(topic = "topic1", partitions = {"0", "1"}),
+            @TopicPartition(topic = "topic2", partitions = "0", partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "5"))},
+            concurrency = "3")
+    public void consume(ConsumerRecord<String, String> record, Acknowledgment ack){
+
+        log.info("record:{}", record);
+        ack.acknowledge();
+    }
 }
